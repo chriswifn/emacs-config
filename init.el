551003d6f0360ec98d8f887b1f0749369d086c1d
Christian Hageloch <christian.hageloch@gmail.com>
2022-12-16 19:21:21 +0100

    remove clear eshell

--- init.el
+++ init.el
@@ -593,23 +593,6 @@
   (let ((cmd (concat "find " (string-join args))))
     (shell-command-to-string cmd)))
 
-(defun run-this-in-eshell (cmd)
-  "Runs the command 'cmd' in eshell."
-  (with-current-buffer "*eshell*"
-    (end-of-buffer)
-    (eshell-kill-input)
-    (message (concat "Running in Eshell: " cmd))
-    (insert cmd)
-    (eshell-send-input)
-    (end-of-buffer)
-    (eshell-bol)
-    (yank)))
-
-(bind-keys*
- ("C-<backspace>" . (lambda () ; clear shell
-                      (interactive)
-                      (run-this-in-eshell "clear 1"))))
-
 (use-package rainbow-mode)
 
 (use-package rg
