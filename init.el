8131e495e5979e2df3a19abed4ae764d79fd31a9
Christian Hageloch <christian.hageloch@gmail.com>
2023-12-15 23:33:14 +0100

    dirty commit for new config

--- init.el
+++ init.el
@@ -1,69 +1,17 @@
-;;; init.el --- Personal configuration file -*- lexical-binding: t; no-byte-compile: t; -*-
-
-;; bootstrap straight.el and install use-package
-(setq straight-use-package-by-default t)
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 6))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
-(straight-use-package 'use-package)
-
-;; setting some sane defaults
-(setq user-full-name "Christian Hageloch")
-(setq ring-bell-function 'ignore)
-(setq use-short-answers t)
-(setq indent-tabs-mode nil)
-(setq blink-cursor-mode nil)
-
-;; file related settings 
-(setq make-backup-files nil)
-(setq auto-save-default nil)
-(setq create-lockfiles nil)
-(setq find-file-visit-truename t)
-(set-default-coding-systems 'utf-8)
-(setq default-process-coding-system '(utf-8-unix . utf-8-unix))
-(global-auto-revert-mode)
-(setq global-auto-revert-non-file-buffers t)
-
-;; line editing
-(setq display-line-numbers-type 'relative)
-(global-hl-line-mode 1)
-
-;; reload the configuration
-(defun chris/config-reload()
-  "Reload the configuration file"
-  (interactive)
-  (load-file (expand-file-name "~/.emacs.d/init.el")))
-(define-key global-map (kbd "C-c r") 'chris/config-reload)
-
-;; custom and abbrev file
-(setq custom-file (locate-user-emacs-file "custom-vars.el"))
-(load custom-file 'noerror 'nomessage)
-(setq abbrev-file-name (locate-user-emacs-file "abbrev.el"))
-
-;; custom configuration for more advanced packages
-(add-to-list 'load-path "~/.emacs.d/chris-lisp")
-(require 'chris-general)
-(require 'chris-evil)
-(require 'chris-buffers)
-(require 'chris-dired)
-(require 'chris-ui)
-(require 'chris-tabbar)
-(require 'chris-nmcli)
-(require 'chris-completion)
-(require 'chris-org)
-(require 'chris-lang)
-(require 'chris-term)
-(require 'chris-treesitter)
-(require 'chris-git)
-(require 'chris-lsp)
-(require 'chris-textcompletion)
-(require 'chris-misc)
+(org-babel-load-file (expand-file-name "~/.emacs.d/README.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("456697e914823ee45365b843c89fbc79191fdbaff471b29aad9dcbe0ee1d5641" "6e33d3dd48bc8ed38fd501e84067d3c74dfabbfc6d345a92e24f39473096da3f" "dccf4a8f1aaf5f24d2ab63af1aa75fd9d535c83377f8e26380162e888be0c6a9" "38c0c668d8ac3841cb9608522ca116067177c92feeabc6f002a27249976d7434" "c8b3d9364302b16318e0f231981e94cbe4806cb5cde5732c3e5c3e05e1472434" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" "e3daa8f18440301f3e54f2093fe15f4fe951986a8628e98dcd781efbec7a46f2" "046a2b81d13afddae309930ef85d458c4f5d278a69448e5a5261a5c78598e012" "871b064b53235facde040f6bdfa28d03d9f4b966d8ce28fb1725313731a2bcc8" "d445c7b530713eac282ecdeea07a8fa59692c83045bf84dd112dd738c7bcad1d" "07885feecd236e4ba3837e7ff15753d47694e1f9a8049400c114b3298285534e" "bfd5296f0c37dc9a52a0416f1d3c380ac381c1da112d061ceb784d0ed51d6587" "0489e00d609dd8d3262d03e2c72b1da0a040c25806c230c6602005be1db01e46" default))
+ '(package-selected-packages
+   '(doom-modeline gruvbox evil-commentary evil-collection evil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-block-begin-line ((t (:background nil))))
+ '(org-block-end-line ((t (:background nil)))))
